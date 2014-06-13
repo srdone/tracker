eb5b75f9d7f74eb7cf9254b91f25b356f6b24be4
Stephen Done <srdone@gmail.com>
2014-06-13 13:45:14 -0600

    Added try blocks and modified function selection logic. Need to fix case where file does not exist so that it preps a file with valid headers.

--- tracker.py
+++ tracker.py
@@ -1,42 +1,48 @@
+#! /usr/bin/env python3
+
 import csv
 import datetime
 
 keep_running = True
 
 def print_completed():
-    with open(r'F:\Python Scripts\tracker.csv', mode='r') as f:
-        reader = csv.DictReader(f)
-        for line in reader:
-            print(line['Report Name'], line['Date Completed'])
+    try:
+        with open('tracker.csv', mode='r') as f:
+            reader = csv.DictReader(f)
+            for line in reader:
+                print(line['Report Name'], line['Date Completed'])
+    except IOError:
+        print('No completed reports could be found. Record some reports and try again')
+
+def complete_report():
+    report_name = input("Please enter the name of the report you completed: ")
+    print(report_name)
+    date = datetime.datetime.now()
+
+    try:
+        with open('tracker.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            
+            writer.writerow([report_name, date])
+            
+        print("Added %s to tracker with completion date %s" % (report_name, date))
+        
+    except IOError:
+        print("There was an IO error. Completion not recorded. Try again.")
 
 def get_status():
     answer = input("Enter 'New Report' to add a new report, 'stop' to stop: ")
     if answer == 'New Report':
-        return True
+        complete_report()
+        get_status()
     elif answer == 'stop':
-        return False
+        exit()
     elif answer == 'Completed Today':
         print_completed()
         get_status()
     else:
         print('That is not a valid answer')
         get_status()
-        
-keep_running = get_status()
 
-while keep_running:
-    report_name = input("Please enter the name of the report you completed: ")
-    print(report_name)
-    #date = input("Please enter today's date: ")
-    date = datetime.datetime.now()
-
-    f = open(r'F:\Python Scripts\tracker.csv', 'a', newline='')
-    writer = csv.writer(f)
-
-    writer.writerow([report_name, date])
-    f.close()
-
-    print("Added %s to tracker with completion date %s" % (report_name, date))
-
-    keep_running = get_status()
-        
+if __name__=='__main__':
+    get_status()
