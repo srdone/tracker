5e90443cb88ef9cc231f23ccaf72c633af6ea8ff
Stephen Done <srdone@gmail.com>
2014-06-23 07:27:47 -0600

    Outine and some details of database calls. No error handling yet, no bugs checked yet. Next task: build class files for analysts and reports.

--- resources/store.py
+++ resources/store.py
@@ -2,22 +2,18 @@ import sqlite3
 
 class ReportDatabase(object):
 	"""docstring for ReportDatabase"""
-	def __init__(self, arg):
-		super(ReportDatabase, self).__init__()
-		self.arg = arg
+	def __init__(self):
+		db = sqlite3.connect('data/mydata')
 		
 	def create_initial_db(self):
-		db = sqlite3.connect('data/mydata')
-
 		cursor = db.cursor()
 		cursor.execute('''
 			CREATE TABLE reports(
 				id INTEGER PRIMARY KEY AUTO_INCREMENT,
 				report_title VARCHAR(30),
 				create_date DATE,
-				next_due_date DATE,
-				recurring BOOLEAN,
-				FOREIGN KEY(owner) REFERENCES analysts(id)
+				next_due_date DATE
+				recurring BOOLEAN
 				)
 			''')
 
@@ -42,7 +38,6 @@ class ReportDatabase(object):
 				id INTEGER PRIMARY KEY AUTO_INCREMENT,
 				first_name VARCHAR(30),
 				last_name VARCHAR(30)
-				FOREIGN KEY(repo)
 				)
 			''')
 
@@ -53,26 +48,112 @@ class ReportDatabase(object):
 				)
 			''')
 
-	def add_analyst(self, first_name, last_name):
-		pass
+	def add_analyst(self, analyst):
+		cursor = db.cursor()
 
-	def add_report(self, report_title, create_date, next_due_date, recurring, owner=None):
-		pass
+		cursor.execute('''
+			INSERT INTO analysts(first_name, last_name)
+			VALUES (?,?)
+			''', (analyst.first_name, analyst.last_name))
 
-	def complete_report():
-		pass
+		db.commit()
+		return True
+
+	def add_report(self, report, analyst):
+		cursor = db.cursor()
+
+		cursor.execute('''
+			INSERT INTO reports(report_title, create_date,
+				next_due_date, recurring) VALUES(?,?,?,?)
+			''', (report.report_title, report.create_date
+				, report.next_due_date, report.recurring)
+
+		cursor.execute('''
+			INSERT INTO reports_owners(owner_id, report_id)
+			VALUES(?,?)
+			''', (analyst.id, report.id))
+
+		db.commit()
+
+	def complete_report(self, report, date):
+		cursor = db.cursor()
+
+		cursor.execute('''
+			UPDATE completion_dates SET complete_date=? WHERE id=?
+			''', (report.id, date))
+
+		cursor.execute()
 
 	def add_notes():
 		pass
 
-	def add_owner():
+	def add_owner(self, report, analyst):
+		cursor = db.cursor()
+
+		cursor.execute('''
+			INSERT INTO reports_owners(owner_id, report_id)
+			VALUES(?,?)
+			''', (analyst.id, report.id))
+
+		db.commit()
+
+	def delete_owner(self, report, analyst):
+		'Delete a line of ownership'
+		cursor = db.cursor()
+
+		cursor.execute('''
+			DELETE FROM reports_owners WHERE owner_id=? AND report_id=?
+			''', (analyst.id, report.report_id))
+
+		cursor.execute()
+
+	def edit_report(self, report):
 		pass
 
-	def edit_owner():
+	def edit_analyst(self, analyst):
 		pass
 
-	def edit_report():
+	def delete_analyst(self, analyst):
+		#delete all ownership lines for reports belonging to this analyst
+		#delete the analyst record
 		pass
 
-	def edit_analyst():
+	def delete_report(self, report):
+		#delete all ownership lines for this report
+		#delete the report record
 		pass
+
+	def get_analysts(self):
+		cursor = db.cursor
+
+		return cursor.execute('''SELECT * FROM analysts''')
+
+	def get_analyst(self, id):
+		cursor = db.cursor
+
+		cursor.execute('''SELECT * FROM analysts WHERE id=?''', id)
+		analyst_row = cursor.fetchone()
+
+		analyst = Analyst(id=analyst_row['id'], first_name=analyst_row['first_name']
+			, last_name=analyst_row['last_name'])
+		
+		analyst.reports.append(get_analyst_reports(analyst))
+
+		return analyst
+
+	def get_analyst_reports(self, analyst):
+		cursor = db.cursor
+
+		report_list = cursor.execute('''SELECT r.*, max(cd.complete_date) AS max_complete_date
+								FROM reports as r
+								LEFT JOIN reports_owners AS ro
+								ON reports.id=reports_owners.report_id 
+								LEFT JOIN completion_dates AS cd
+								ON reports.id=completion_dates.report_id
+								WHERE analysts.id=?
+								GROUP BY reports.*''', analyst.id)
+
+		for line in report_list:
+			reports.append(Report(report_id=line['id'], report_title=line['report_title']
+				, create_date=line['create_date'], next_due_date=line['next_due_date']
+				, recurring=line['recurring'], max_complete_date=line['max_complete_date']))
